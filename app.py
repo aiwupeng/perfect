8ca6095ad7303ebaf9abe8435e304f3534786853
aiwupeng <1204336293@qq.com>
2025-05-03 15:34:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 # coding=utf-8
 import streamlit as st
 import pandas as pd
-import akshare as ak
 import matplotlib.pyplot as plt
 import os
 from matplotlib.font_manager import FontProperties
@@ -85,34 +84,6 @@ def visualization(stkcd_data):
             ax.legend()
             # 绘图逻辑...
             st.pyplot(fig)
-def fund_flow_query(stock_code, market):
-    try:
-        stock_individual_fund_flow_df = ak.stock_individual_fund_flow(stock=stock_code, market=market)
-        return stock_individual_fund_flow_df
-    except Exception as e:
-        st.error(f'查询失败：{e}')
-        return None
-def get_top_ten_free_holders(stock_code, date):
-    """
-    获取指定股票和日期的十大流通股东持仓情况。
-    参数：
-    stock_code (str): 股票代码。
-    date (str): 财报发布季度最后日，格式为YYYYMMDD。
-    返回：
-    pd.DataFrame: 十大流通股东持仓情况的数据框。
-    """
-    try:
-        # 使用AKShare获取所有股票的十大流通股东数据
-        all_holders_df = ak.stock_gdfx_free_holding_analyse_em(date=date)
-
-        # 筛选出特定股票的十大流通股东数据
-        specific_stock_holders_df = all_holders_df[all_holders_df['股票代码'] == stock_code]
-
-        return specific_stock_holders_df
-    except Exception as e:
-        st.error(f"获取数据时发生错误：{e}")
-        return pd.DataFrame()
-
 def main():
     st.title("彭博士公司评分系统")
     df = load_data()
@@ -148,33 +119,7 @@ def main():
             ['Name', 'IndName', 'Year', 'Cfo_core', 'Cfo', 'Buy_cash', 'Invest_cash', 'Free_cash',
              'Excess_cash', 'Finance_cash_in', 'Oper_revenue']]
     st.dataframe(columns_to_display4)
-    st.title('个股资金流向数据查询')
-    stock_code = st.text_input('输入股票代码', '002139')
-    market_type = st.selectbox('选择市场类型', ('上海证券交易所', '深圳证券交易所'), index=0)
-    market = 'sh' if market_type == '上海证券交易所' else 'sz'
-    if st.button('查询资金流向'):
-        fund_flow_df = fund_flow_query(stock_code, market)
-        if fund_flow_df is not None:
-            st.write('查询结果：')
-            st.dataframe(fund_flow_df)
-
-    # 页面标题
-    st.title('个股十大流通股东持仓情况')
-    # 创建一个文本输入框，让用户输入股票代码
-    stock_code = st.text_input('请输入股票代码', '002139')  # 以贵州茅台为例，默认输入600519
-    # 创建一个日期选择器，让用户选择财报发布季度最后日
-    date = st.date_input('请选择财报发布季度最后日', value=None)
-    # 检查输入是否有效，并获取数据
-    if st.button('获取数据'):
-        if stock_code and date:
-            # 调用函数获取数据
-            result_df = get_top_ten_free_holders(stock_code, date.strftime('%Y%m%d'))
-            # 展示数据
-            st.write("个股十大流通股东持仓情况：")
-            st.dataframe(result_df)
-        else:
-            st.error("请输入有效的股票代码和日期")
-
+  
 
 if __name__ == "__main__":
     main()
